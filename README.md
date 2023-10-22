79c306215def24263b13954b2cdecd9be2733434
cybertron team <cybertron@collabothon-2023>
2023-10-22 12:01:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# collabothon-2023-cybertron
+demo repo used from Cybertron team during Collabothon event - 2023 - Sofia
+
+Read.me file of the project: https://hackmd.io/qnI6Viq2SciXLyIhuMy-yA?view 
---
+++ collabothon/docker/unsigned/public-html/index.php
@@ -0,0 +1,17 @@
+<html>
+<style>
+img {
+    padding: 0;
+    display: block;
+    margin: 0 auto;
+    max-height: 50%;
+    max-width: 50%;
+}
+</style>
+<title>Cybetron team - Collabothon 2023</title>
+<img src=collabothon.png class="center">
+
+<center><h1>This container is running on <?php echo gethostname(); ?> </h1></center>
+<br><br>
+<img src=unsigned.png>
+</html>
